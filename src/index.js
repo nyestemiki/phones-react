4db677f6af9884f503ede2baa9c089a325a7b953
nyestemiki <nyeste.miki@yahoo.com>
2019-08-20 21:37:47 +0100

    Bug fixes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 // import Router from './components/Router'
-// import './css/style.css';
+import App from './components/App';
 
-render(<Router />, document.querySelector('#main'));
+render(<App />, document.querySelector('#main'));
