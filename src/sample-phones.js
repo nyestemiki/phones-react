0d20ad2fc160296f83b403acb09228a5f9f57a78
nyestemiki <nyeste.miki@yahoo.com>
2019-08-21 10:10:04 +0100

    Sample phones

---
+++ src/sample-phones.js
@@ -0,0 +1,110 @@
+const brands = {
+    Samsung: {
+        brandname: "Samsung",
+        covermodel: "Galaxy S8",
+        cover: "imgs/Samsung/s8.png",
+        modelList: {
+            Galaxy_S8: {
+                img: "imgs/Samsung/s8.png"
+            },
+            Galaxy_S10Plus: {
+                img: "imgs/Samsung/s10+.png",
+                body: "157.6 x 74.1 x 7.8mm, 175g",
+                display: "Amoled, 6.4\", 1440x3040px, 19:9, Gorilla Glass 6",
+                os: "Android 9.0",
+                processor: "Snapdragon 855, 8 cores",
+                ram: "12 GB",
+                storage: "1 TB",
+                camera: "12, 12, 16 MP",
+                selfie: "10, 8 MP",
+                battery: "4100 mAh"
+            },
+            Galaxy_A40: {
+                img: "imgs/Samsung/a40.png"
+            },
+            Galaxy_A70: {
+                img: "imgs/Samsung/a70.png"
+            },
+            Galaxy_M20: {
+                img: "imgs/Samsung/m20.png"
+            },
+            Galaxy_S9: {
+                img: "imgs/Samsung/s9.png"
+            },
+            Galaxy_S10e: {
+                "img": "imgs/Samsung/s10e.png"
+            }
+        }
+    },
+    Apple: {
+        brandname: "Apple",
+        covermodel: "iPhone X",
+        cover: "imgs/Apple/x.png",
+        modelList: {
+            iPhone_X: {
+                img: "imgs/Apple/x.png"
+            },
+            iPhone_Xs_Max: {
+                img: "imgs/Apple/xsmax.png"
+            },
+            iPhone_7: {
+                img: "imgs/Apple/7.png"
+            },
+            iPhone_8: {
+                img: "imgs/Apple/8.png",
+                body: "138.4 x 76.3 x 7.3mm, 148g",
+                display: "IPS LCD, 4.7\" 750x1334px, 16:9, Oleophobic Coating",
+                os: "iOS 11",
+                processor: "A11 Bionic, 6 cores",
+                ram: "2 GB",
+                storage: "256 GB",
+                camera: "12 MP",
+                selfie: "7 MP",
+                battery: "1821 mAh"
+            },
+            iPhone_Xr: {
+                img: "imgs/Apple/xr.png"
+            },
+            iPhone_Xs: {
+                "img": "imgs/Apple/xs.png"
+            }
+        }
+    },
+    OnePlus: {
+        brandname: "OnePlus",
+        covermodel: "One+ 7Pro",
+        cover: "imgs/OnePlus/7pro.png",
+        modelList: {
+            OnePlus_7Pro: {
+                img: "imgs/OnePlus/7pro.png",
+                body: "162.6 x 75.9 x 8.8mm, 206g",
+                display: "Amoled, 6.67\", 1440x3120px, 19.5:9, Gorilla Glass 5",
+                os: "Android 9.0",
+                processor: "Snapdragon 855, 8 cores",
+                ram: "12 GB",
+                storage: "256 GB",
+                camera: "48, 8, 16 MP",
+                selfie: "16 MP",
+                battery: "4000 mAh"
+            },
+            OnePlus_6T: {
+                img: "imgs/OnePlus/6t.png"
+            },
+            OnePlus_5: {
+                img: "imgs/OnePlus/5.png"
+            },
+            OnePlus_5T: {
+                img: "imgs/OnePlus/5t.png"
+            },
+
+            OnePlus_6T_Maclaren: {
+                img: "imgs/OnePlus/6tmaclaren.png"
+            },
+            OnePlus_7: {
+                img: "imgs/OnePlus/7.png"
+            }
+        }
+    }
+};
+
+export default brands;
