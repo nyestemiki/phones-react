cc5046a1e742c23e33bc02e7f5da5f4f0454c834
nyestemiki <nyeste.miki@yahoo.com>
2019-09-17 23:59:38 +0300

    Select brand improvements (styling & refactoring)

--- src/components/App.js
+++ src/components/App.js
@@ -4,49 +4,70 @@ import samplePhones from '../sample-phones';
 import Navbar from './Navbar';
 
 class App extends React.Component {
-    state = {
-        selectedBrand: ''
-    }
-
     // Selects the chosen brand
     selectBrand = brand => {
-        this.setState({selectedBrand: brand.brandname});
+        const allBrands = document.querySelectorAll('.brand'); // Nodelist
+        const allBrandsArray = Array.from(allBrands);
+        
+        // Removing the hover effect from all brand divs
+        allBrandsArray.forEach(div => {
+            div.classList.remove("brand_hover");
+        });
+
+        // Fading out the unselected brand divs
+        allBrandsArray
+            .filter(div => div.id !== brand.brandname)
+            .forEach(div => {
+                div.classList.add("fade-out-brand");
+                div.classList.add("fade-out-brand-img");
+                div.classList.add("fade-out-brand-title");
+            });
     }
 
+    // Redirects to the main menu
     mainMenu = () => {
-        this.setState({selectedBrand: ''});
+        const allBrands = document.querySelectorAll('.brand'); // Nodelist
+        const allBrandsArray = Array.from(allBrands);
+    
+        // Fading in the hidden brand divs
+        allBrandsArray
+            .forEach(div => {
+                div.classList.remove("fade-out-brand");
+            });
+
+        // Displaying the cover images and titles
+        setTimeout(() => {
+            allBrandsArray
+                .forEach(div => {
+                    div.classList.remove("fade-out-brand-img");
+                    div.classList.remove("fade-out-brand-title");
+                });
+        }, 500);
+
+        // Reapplying the hover effect
+        setTimeout(() => {
+            allBrandsArray
+                .forEach(div => {
+                    div.classList.add("brand_hover");
+                });
+        }, 760); // 750 ms transition
     }
 
     render() {
         return (
             <>
                 <Navbar mainMenu={this.mainMenu}/>
-                <div className="brands">
-                    { 
-                        !this.state.selectedBrand && (
-                            Object
-                                .keys(samplePhones)
-                                .map(key => 
-                                    <Brand 
-                                        brand={samplePhones[key]} 
-                                        key={key}
-                                        id={key}
-                                        selectBrand={this.selectBrand}
-                                    />) 
-                        ) ||  (
-                            Object
-                                .keys(samplePhones)
-                                .filter(brand => brand === this.state.selectedBrand)
-                                .map(key => 
-                                    <Brand 
-                                        brand={samplePhones[key]} 
-                                        key={key}
-                                        id={key}
-                                        selectBrand={this.selectBrand}
-                                    />) 
-                        )
-                    }
-                </div>
+                <div className="brands"> {   
+                    Object.keys(samplePhones)
+                        .map(key => 
+                            <Brand 
+                                brand={samplePhones[key]} 
+                                key={key}
+                                id={key}
+                                selectBrand={this.selectBrand}
+                            />
+                        ) 
+                } </div>
             </>
         );
     }
